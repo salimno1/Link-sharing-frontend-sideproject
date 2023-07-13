1eb20249eb3f1e4738813858ffd671b3854d3311
Salim Mvoungui <salim.mvoungui@gmail.com>
2023-07-13 14:54:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import LeftBar from "../components/leftBar/leftBar";
+
+import RightBar from "../components/rightBar/RightBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from "../components/navBar/Navbar";
+function App() {
+  const currentUser = true;
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return (
+    <div>
+      {" "}
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
